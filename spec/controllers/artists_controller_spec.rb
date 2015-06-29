3fa91ed2805cacc6138a5de271e78988023f1a85
askl56 <amscott@openmailbox.org>
2015-06-29 23:31:06 +0100

    got tests to pass

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe ArtistsController, type: :controller do
   describe "GET show" do
     it "returns http success" do
       sign_in
-      get :show, artist: "50 Cent"
+      get :show, id: "50 Cent"
       expect(response).to have_http_status(:success)
     end
   end
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -9,3 +9,8 @@ module ControllerHelpers
     end
   end
 end
+
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+  config.include ControllerHelpers, type: :controller
+end
