81db1ea31e06bddc41905745763a961ad89cd827
askl56 <amscott@openmailbox.org>
2015-06-29 18:10:09 +0100

    added basic controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- get '/artists', to: 'artists#index'
- get '/artists/:id', to:'artists#show', as: 'artist'
+  get '/artists', to: 'artists#index'
+  get '/artists/:id', to:'artists#show', as: 'artist'
 
   root to: 'visitors#index'
   devise_for :users
