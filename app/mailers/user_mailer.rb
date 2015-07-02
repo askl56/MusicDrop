f363d609677a0c685ced76b4e839cfef980eac20
askl56 <amscott@openmailbox.org>
2015-07-02 14:22:06 +0100

    added invitation instructions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to Mark1')
+    mail(to: @user.email, subject: 'Welcome to MusicDrop')
   end
 end
