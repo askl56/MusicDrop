5a0938d8c51e1c269f044568580a73199a9487a6
askl56 <amscott@openmailbox.org>
2015-07-03 11:46:22 +0100

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, if: :new_record?
+
+  has_many :topics
+  has_many :bookmarks, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_bookmarks, through: :likes, source: :bookmark
+
+  def pending_invite?
+    invitation_created_at && !invitation_accepted_at
+  end
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+
 end
