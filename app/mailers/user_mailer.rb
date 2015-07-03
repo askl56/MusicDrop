d06afc2b0a6e1a4061b37ae36ccc37dbc995df60
askl56 <amscott@openmailbox.org>
2015-07-03 11:52:18 +0100

    Revert "added invitation instructions"
    This reverts commit f363d609677a0c685ced76b4e839cfef980eac20.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to MusicDrop')
+    mail(to: @user.email, subject: 'Welcome to Mark1')
   end
 end
